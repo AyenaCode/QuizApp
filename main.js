9a4d0d7e0171d16fdca1dcc20ebbb1cacb3900e0
AyenaCode <ayenacode1@gmail.com>
2024-10-02 10:56:19 -0400

    reduce

--- main.js
+++ main.js
@@ -21,91 +21,91 @@ const questions = [
     ],
     answer: "243 milliards",
   },
-  {
-    question: "Quelle est le continet le plus grand du monde ?",
-    choices: ["Afrique", "Europe", "Asie", "Amerique"],
-    answer: "Asie",
-  },
-  {
-    question: "Quel est le plus grand pays du monde ?",
-    choices: ["Russie", "Canada", "Chine", "Etats-Unis"],
-    answer: "Russie",
-  },
-  {
-    question: "Quelle est sa capitale ?",
-    choices: ["Moscou", "Paris", "Berlin", "Londres"],
-    answer: "Moscou",
-  },
-  {
-    question: "Quel est le plus petit pays du monde ?",
-    choices: ["Monaco", "Vatican", "Malte", "Liechtenstein"],
-    answer: "Vatican",
-  },
-  {
-    question: "Quelle est sa superficie ?",
-    choices: ["0,44 km²", "0,49 km²", "0,55 km²", "0,61 km²"],
-    answer: "0,44 km²",
-  },
-  {
-    question: "Quelle est sa monnaie locale ?",
-    choices: ["Euro", "Dollar", "Franc Suisse", "Lire"],
-    answer: "Euro",
-  },
-  {
-    question: "Quel est le plus grand océan du monde ?",
-    choices: ["Atlantique", "Pacifique", "Indien", "Arctique"],
-    answer: "Pacifique",
-  },
-  {
-    question: "Quelle monnaie est la plus échangée dans le monde ?",
-    choices: ["Dollar", "Euro", "Yen", "Franc Suisse"],
-    answer: "Dollar",
-  },
-  {
-    question: "Quelle est la monnaie la plus utiliser en Afrique ?",
-    choices: ["Euro", "Dollar", "Franc CFA", "Franc Suisse"],
-    answer: "Franc CFA",
-  },
-  {
-    question: "Quelle est la monnaie la plus utiliser en Asie ?",
-    choices: ["Euro", "Dollar", "Yen", "Franc Suisse"],
-    answer: "Yen",
-  },
-  {
-    question: "Quelle est la monnaie la plus utiliser en Europe ?",
-    choices: ["Euro", "Dollar", "Yen", "Franc Suisse"],
-    answer: "Euro",
-  },
-  {
-    question: "Quelle est la monnaie la plus utiliser en Amérique ?",
-    choices: ["Euro", "Dollar", "Yen", "Franc Suisse"],
-    answer: "Dollar",
-  },
-  {
-    question: "Quelle est la monnaie de la côte d'ivoire ?",
-    choices: ["Euro", "Dollar", "Franc CFA", "Franc Suisse"],
-    answer: "Franc CFA",
-  },
-  {
-    question: "Quelle est la monnaie du Togo ?",
-    choices: ["Euro", "Dollar", "Franc CFA", "Franc Suisse"],
-    answer: "Franc CFA",
-  },
-  {
-    question: "Quelle est la monnaie du Maroc ?",
-    choices: ["Euro", "Dollar", "Franc CFA", "Franc Suisse"],
-    answer: "Franc CFA",
-  },
-  {
-    question: "Quelle est la monnaie du Ghana ?",
-    choices: ["Euro", "Dollar", "Franc CFA", "Cedi"],
-    answer: "Cedi",
-  },
-  {
-    question: "Quelle est la monnaie du Burkina Faso ?",
-    choices: ["Euro", "Dollar", "Franc CFA", "Franc Suisse"],
-    answer: "Franc CFA",
-  },
+  // {
+  //   question: "Quelle est le continet le plus grand du monde ?",
+  //   choices: ["Afrique", "Europe", "Asie", "Amerique"],
+  //   answer: "Asie",
+  // },
+  // {
+  //   question: "Quel est le plus grand pays du monde ?",
+  //   choices: ["Russie", "Canada", "Chine", "Etats-Unis"],
+  //   answer: "Russie",
+  // },
+  // {
+  //   question: "Quelle est sa capitale ?",
+  //   choices: ["Moscou", "Paris", "Berlin", "Londres"],
+  //   answer: "Moscou",
+  // },
+  // {
+  //   question: "Quel est le plus petit pays du monde ?",
+  //   choices: ["Monaco", "Vatican", "Malte", "Liechtenstein"],
+  //   answer: "Vatican",
+  // },
+  // {
+  //   question: "Quelle est sa superficie ?",
+  //   choices: ["0,44 km²", "0,49 km²", "0,55 km²", "0,61 km²"],
+  //   answer: "0,44 km²",
+  // },
+  // {
+  //   question: "Quelle est sa monnaie locale ?",
+  //   choices: ["Euro", "Dollar", "Franc Suisse", "Lire"],
+  //   answer: "Euro",
+  // },
+  // {
+  //   question: "Quel est le plus grand océan du monde ?",
+  //   choices: ["Atlantique", "Pacifique", "Indien", "Arctique"],
+  //   answer: "Pacifique",
+  // },
+  // {
+  //   question: "Quelle monnaie est la plus échangée dans le monde ?",
+  //   choices: ["Dollar", "Euro", "Yen", "Franc Suisse"],
+  //   answer: "Dollar",
+  // },
+  // {
+  //   question: "Quelle est la monnaie la plus utiliser en Afrique ?",
+  //   choices: ["Euro", "Dollar", "Franc CFA", "Franc Suisse"],
+  //   answer: "Franc CFA",
+  // },
+  // {
+  //   question: "Quelle est la monnaie la plus utiliser en Asie ?",
+  //   choices: ["Euro", "Dollar", "Yen", "Franc Suisse"],
+  //   answer: "Yen",
+  // },
+  // {
+  //   question: "Quelle est la monnaie la plus utiliser en Europe ?",
+  //   choices: ["Euro", "Dollar", "Yen", "Franc Suisse"],
+  //   answer: "Euro",
+  // },
+  // {
+  //   question: "Quelle est la monnaie la plus utiliser en Amérique ?",
+  //   choices: ["Euro", "Dollar", "Yen", "Franc Suisse"],
+  //   answer: "Dollar",
+  // },
+  // {
+  //   question: "Quelle est la monnaie de la côte d'ivoire ?",
+  //   choices: ["Euro", "Dollar", "Franc CFA", "Franc Suisse"],
+  //   answer: "Franc CFA",
+  // },
+  // {
+  //   question: "Quelle est la monnaie du Togo ?",
+  //   choices: ["Euro", "Dollar", "Franc CFA", "Franc Suisse"],
+  //   answer: "Franc CFA",
+  // },
+  // {
+  //   question: "Quelle est la monnaie du Maroc ?",
+  //   choices: ["Euro", "Dollar", "Franc CFA", "Franc Suisse"],
+  //   answer: "Franc CFA",
+  // },
+  // {
+  //   question: "Quelle est la monnaie du Ghana ?",
+  //   choices: ["Euro", "Dollar", "Franc CFA", "Cedi"],
+  //   answer: "Cedi",
+  // },
+  // {
+  //   question: "Quelle est la monnaie du Burkina Faso ?",
+  //   choices: ["Euro", "Dollar", "Franc CFA", "Franc Suisse"],
+  //   answer: "Franc CFA",
+  // },
 ];
 
 const start = document.getElementById("start");
